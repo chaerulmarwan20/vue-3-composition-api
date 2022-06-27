71f3f8a5b06b9597e3b37813096c288fa8964ba0
Marwan <chaerul.marwan@weekendinc.com>
2022-06-27 09:58:32 +0700

    ref

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>{{ message }}</div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const message = ref("Hello World");
+
+    setTimeout(() => {
+      message.value = "Halo Dunia";
+    }, 2000);
+
+    return {
+      message,
+    };
+  },
+};
+</script>
