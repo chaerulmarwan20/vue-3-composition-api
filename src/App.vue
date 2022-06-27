858e5b1a4eb8895cb5321abe80b60bd0bf86b589
Marwan <chaerul.marwan@weekendinc.com>
2022-06-27 10:23:10 +0700

    computed

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,29 @@
 <template>
   <div>Count : {{ nilai }}</div>
   <button type="button" @click="add">Add</button>
+  <div>Result : {{ result }}</div>
 </template>
 
 <script>
-import { reactive, toRefs } from "vue";
+import { reactive, ref, computed, toRefs } from "vue";
 
 export default {
   setup() {
     const counter = reactive({
       nilai: 0,
     });
+    const addNum = ref(1);
 
     const add = () => {
       counter.nilai++;
     };
 
+    const result = computed(() => counter.nilai + addNum.value);
+
     return {
       ...toRefs(counter),
       add,
+      result,
     };
   },
 };
