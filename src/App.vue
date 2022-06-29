827dcd192d7f7563d60e0f487cc8f9435f979767
Marwan <chaerul.marwan@weekendinc.com>
2022-06-29 11:07:30 +0700

    simple todo app

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,81 @@
 <template>
-  <user-component :label="label" :user="user" class="active" @submit="onSubmit">
-    <div>Slot</div>
-  </user-component>
-  <button type="button" @click="changeName">Change</button>
+  <div class="container" style="margin-top: 20px">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">SIMPLE TODO APP</h5>
+        <div class="row">
+          <div class="col-10">
+            <input
+              type="text"
+              class="form-control"
+              v-model="todo"
+              @keyup.enter="add"
+            />
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-success" @click="add">
+              ADD
+            </button>
+          </div>
+        </div>
+        <list :todos="list" @deleteTodo="deleteTodo" @doneTodo="doneTodo" />
+        <br />
+        <small>Total TODO : {{ totalTodo }}</small>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { ref, reactive } from "vue";
-import UserComponent from "./components/UserComponent.vue";
+import { ref, reactive, computed, toRefs, onMounted, toRef } from "vue";
+import List from "./components/List.vue";
 
 export default {
-  components: { UserComponent },
+  components: { List },
   setup() {
-    const label = ref("biodata");
-    const user = reactive({
-      name: "Chaerul",
-      age: 22,
+    const todo = ref("");
+    const todos = reactive({
+      list: [],
     });
 
-    const changeName = () => {
-      user.name = "Marwan";
+    const totalTodo = computed(() => {
+      return todos.list.length;
+    });
+
+    const add = () => {
+      todos.list.unshift({ activity: todo.value, isDone: false });
+      todo.value = "";
+      saveToLocalStorage();
     };
 
-    const onSubmit = (val) => {
-      console.log(val);
+    const deleteTodo = (todoIdx) => {
+      todos.list = todos.list.filter((item, idx) => {
+        if (idx !== todoIdx) {
+          return item;
+        }
+      });
+      saveToLocalStorage();
     };
 
-    return {
-      label,
-      user,
-      changeName,
-      onSubmit,
+    const doneTodo = (todoIdx) => {
+      todos.list = todos.list.filter((item, idx) => {
+        if (idx === todoIdx) {
+          item.isDone = true;
+        }
+        return item;
+      });
+      saveToLocalStorage();
     };
+
+    const saveToLocalStorage = () => {
+      localStorage.setItem("todos", JSON.stringify(todos.list));
+    };
+
+    onMounted(() => {
+      todos.list = JSON.parse(localStorage.getItem("todos") || []);
+    });
+
+    return { todo, ...toRefs(todos), totalTodo, add, deleteTodo, doneTodo };
   },
 };
 </script>
---
+++ src/components/List.vue
@@ -0,0 +1,58 @@
+<template>
+  <div style="margin-top: 20px">
+    <ul class="list-group">
+      <li class="list-group-item" v-for="(todo, idx) in todos" :key="idx">
+        <div class="row">
+          <div class="col">
+            <span v-if="todo.isDone">
+              <del>{{ todo.activity }}</del>
+            </span>
+            <span v-else>
+              {{ todo.activity }}
+            </span>
+          </div>
+          <div class="col-auto">
+            <div class="row gx-2">
+              <div class="col">
+                <button type="button" class="btn btn-info" @click="done(idx)">
+                  Done
+                </button>
+              </div>
+              <div class="col">
+                <button
+                  type="button"
+                  class="btn btn-danger"
+                  @click="deleteTodo(idx)"
+                >
+                  X
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todos: {
+      type: Array,
+      default: [],
+    },
+  },
+  setup(props, { emit }) {
+    const deleteTodo = (idx) => {
+      emit("deleteTodo", idx);
+    };
+
+    const done = (idx) => {
+      emit("doneTodo", idx);
+    };
+
+    return { deleteTodo, done };
+  },
+};
+</script>
